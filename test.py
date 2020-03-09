a6c4b5f2f877497fd409c15c835f615a227146e5
Mikey McCormack <michaeljohnmccormack@gmail.com>
2020-03-08 20:09:27 -0400

    Add flask and some endpoints that call out to yfinance

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
+from flask import Flask
 import yfinance as yf
+import json
+from pandas import Timestamp
 
-msft = yf.Ticker("MSFT")
+app = Flask(__name__)
 
-file = open("./source_files/nasdaqlisted.txt")
-print(file.readline())
 
+@app.route("/tickers/<string:ticker>/info")
+def info(ticker):
+    return yf.Ticker(ticker).info
+
+
+@app.route("/tickers/<string:ticker>/institutional-holders")
+def institutional_holders(ticker):
+    response = yf.Ticker(ticker).institutional_holders.values.tolist()
+    response_dict = {"institutional_holders": response}
+    return json.dumps(response_dict, default=converter)
+
+
+def converter(obj):
+    if isinstance(obj, Timestamp):
+        return obj.__str__()
+
+
+if __name__ == "__main__":
+    app.run()

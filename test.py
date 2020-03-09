72f102fcc31211ffa55ac1e0e2a018c8d61aa721
Mikey McCormack <michaeljohnmccormack@gmail.com>
2020-03-08 22:34:16 -0400

    Round out REST endpoints for a single ticker. Don't yet combine all attributes for a single ticker. Don't yet handle multiple tickers.

--- test.py
+++ test.py
@@ -13,9 +13,87 @@ def info(ticker):
 
 @app.route("/tickers/<string:ticker>/institutional-holders")
 def institutional_holders(ticker):
-    response = yf.Ticker(ticker).institutional_holders.values.tolist()
-    response_dict = {"institutional_holders": response}
-    return json.dumps(response_dict, default=converter)
+    response = yf.Ticker(ticker).institutional_holders
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/actions")
+def actions(ticker):
+    response = yf.Ticker(ticker).actions
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/dividends")
+def dividends(ticker):
+    response = yf.Ticker(ticker).dividends
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/splits")
+def splits(ticker):
+    response = yf.Ticker(ticker).splits
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/major-holders")
+def major_holders(ticker):
+    response = yf.Ticker(ticker).major_holders
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/cashflow")
+def cashflow(ticker):
+    response = yf.Ticker(ticker).cashflow
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/quarterly-cashflow")
+def quarterly_cashflow(ticker):
+    response = yf.Ticker(ticker).quarterly_cashflow
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/earnings")
+def earnings(ticker):
+    response = yf.Ticker(ticker).earnings
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/quarterly-earnings")
+def quarterly_earnings(ticker):
+    response = yf.Ticker(ticker).quarterly_earnings
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/sustainability")
+def sustainability(ticker):
+    response = yf.Ticker(ticker).sustainability
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/recommendations")
+def recommendations(ticker):
+    response = yf.Ticker(ticker).recommendations
+    response.reset_index(drop=True, inplace=True)
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/calendar")
+def calendar(ticker):
+    response = yf.Ticker(ticker).calendar
+    return response.to_json()
+
+
+@app.route("/tickers/<string:ticker>/isin")
+def isin(ticker):
+    response = {"isin": yf.Ticker(ticker).isin}
+    return response
+
+
+@app.route("/tickers/<string:ticker>/options")
+def options(ticker):
+    response = {"options": list(yf.Ticker(ticker).options)}
+    return response
 
 
 def converter(obj):
